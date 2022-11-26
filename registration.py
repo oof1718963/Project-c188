84b0489685d6295712fbbe95a421fce26dd4e05a
oof1718963 <75444840+oof1718963@users.noreply.github.com>
2022-11-26 11:13:28 -0500

    Update registration.py

--- registration.py
+++ registration.py
@@ -55,44 +55,28 @@ def login_window():
     login_window.mainloop()
 
 
-def login():
-    global login_username_entry1
-    global login_password_entry1
-    user_name = login_username_entry1
-    password = login_password_entry1
-
-    encrypted_password = hashlib.md5(password.encode())
-    hexadecimal_password = encrypted_password.hexdigest()
-    get_password = firebase.get("/", user_name)
-    print(hexadecimal_password)
-    if get_password != None:
-        if get_password == hexadecimal_password:
-            messagebox.showinfo("Information", "Successfully logged in")
-        else:
-            messagebox.showinfo("Information", "Please check your password.")
-    else:
-        messagebox.showinfo("Information", "User not registered \n To login, get yourself registered first")
 
 
-heading_label = Label(registration_window, text="Register", font='arial 18 bold')
+
+heading_label = Label(registration_window, text="Register", font='sf pro display 18 bold')
 heading_label.place(relx=0.5, rely=0.2, anchor=CENTER)
 
-username_label = Label(registration_window, text="Username : ", font='arial 13')
+username_label = Label(registration_window, text="Username : ", font='as pro display 13')
 username_label.place(relx=0.3, rely=0.4, anchor=CENTER)
 
 username_entry = Entry(registration_window)
 username_entry.place(relx=0.6, rely=0.4, anchor=CENTER)
 
-password_label = Label(registration_window, text="Password :  ", font='arial 13')
+password_label = Label(registration_window, text="Password :  ", font='sf pro display 13')
 password_label.place(relx=0.3, rely=0.5, anchor=CENTER)
 
 password_entry = Entry(registration_window)
 password_entry.place(relx=0.6, rely=0.5, anchor=CENTER)
 
-btn_reg = Button(registration_window, text="Sign Up", font='arial 13 bold', command=register, relief=FLAT, padx=10)
+btn_reg = Button(registration_window, text="Sign Up", font='sf pro display 13 bold', command=register, relief=FLAT, padx=10)
 btn_reg.place(relx=0.5, rely=0.75, anchor=CENTER)
 
-btn_login_window = Button(registration_window, text="Log In", font='arial 10 bold', command=login_window, relief=FLAT)
+btn_login_window = Button(registration_window, text="Log In", font='sf pro display 10 bold', command=login_window, relief=FLAT)
 
 btn_login_window.place(relx=0.9, rely=0.06, anchor=CENTER)
 registration_window.mainloop()
