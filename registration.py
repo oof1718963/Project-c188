8938c78fbf298fd53af296da54c8e87da68068a5
oof1718963 <75444840+oof1718963@users.noreply.github.com>
2022-11-26 10:53:05 -0500

    Add files via upload

---
+++ registration.py
@@ -0,0 +1,60 @@
+import hashlib 
+from tkinter import *
+from firebase import firebase
+
+registration_window = Tk()
+registration_window.geometry("400x400")
+
+def login(): 
+    print("login function")
+    
+def register(): 
+    print("register function")
+    
+def login_window():
+    login_window = Tk()
+    login_window.geometry("400x400")
+    
+    log_heading_label = Label(login_window, text="Log In" , font = 'arial 18 bold')
+    log_heading_label.place(relx=0.5,rely=0.2, anchor=CENTER)
+    
+    login_username_label = Label(login_window, text="Username : " , font = 'arial 13')
+    login_username_label.place(relx=0.3,rely=0.4, anchor=CENTER)
+    
+    login_username_entry = Entry(login_window)
+    login_username_entry.place(relx=0.6,rely=0.4, anchor=CENTER)
+    
+    login_password_label = Label(login_window, text="Password : " , font = 'arial 13')
+    login_password_label.place(relx=0.3,rely=0.5, anchor=CENTER)
+    
+    login_password_entry = Entry(login_window)
+    login_password_entry.place(relx=0.6,rely=0.5, anchor=CENTER)
+    
+    btn_login = Button(login_window, text="Log In" , font = 'arial 13 bold' , command=login, relief=FLAT)
+    btn_login.place(relx=0.5,rely=0.65, anchor=CENTER)
+    
+    login_window.mainloop()
+    
+    
+heading_label = Label(registration_window, text="Register" , font = 'arial 18 bold')
+heading_label.place(relx=0.5,rely=0.2, anchor=CENTER)
+
+username_label = Label(registration_window, text="Username : " , font = 'arial 13')
+username_label.place(relx=0.3,rely=0.4, anchor=CENTER)
+
+username_entry = Entry(registration_window)
+username_entry.place(relx=0.6,rely=0.4, anchor=CENTER)
+
+password_label = Label(registration_window, text="Password :  " , font = 'arial 13')
+password_label.place(relx=0.3,rely=0.5, anchor=CENTER)
+
+password_entry = Entry(registration_window)
+password_entry.place(relx=0.6,rely=0.5, anchor=CENTER)
+
+btn_reg = Button(registration_window, text="Sign Up" , font = 'arial 13 bold' ,command=register, relief=FLAT, padx=10)
+btn_reg.place(relx=0.5,rely=0.75, anchor=CENTER)
+
+btn_login_window = Button(registration_window, text="Log In" , font = 'arial 10 bold' ,  command=login_window, relief=FLAT)
+
+btn_login_window.place(relx=0.9,rely=0.06, anchor=CENTER)
+registration_window.mainloop()
